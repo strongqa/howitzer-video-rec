8c4bdb4cc21feeb7d985c1828f00a02b2e336c4b
lecik <lec1k2103@gmail.com>
2017-12-19 16:56:57 +0200

    version 0.0.1

---
+++ lib/howitzer/video/rec.rb
@@ -0,0 +1,9 @@
+require "howitzer/video/rec/version"
+
+module Howitzer
+  module Video
+    module Rec
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/howitzer/video/rec/version.rb
@@ -0,0 +1,7 @@
+module Howitzer
+  module Video
+    module Rec
+      VERSION = "0.1.0"
+    end
+  end
+end
